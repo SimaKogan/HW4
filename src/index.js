3ab82803c1f0ddf2fa7f0ce3a9185ef45d5a7eb5
SimaKogan <simas96@mail.ru>
2022-10-26 01:25:28 +0300

    HW_10

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
-const detailsTitale = document.querySelector(".details-titale");
+const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
+const detailsAudio = document.querySelector(".details-audio");
 const HIDDEN = "hidden";
+const button = document.querySelector(".hide-button");
+const IS_POINT = "is-point";
 function setDetails(anchor) {
-    detailsImage.setAttribute("src",anchor.getAttribute("data-details-image"));
-    detailsTitale.innerHTML = anchor.getAttribute("data-details-title");
+    detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
+    detailsAudio.setAttribute('src', anchor.getAttribute('data-details-audio'));
+    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
-for(let i = 0; i < anchors.length; i++) {
+
+
+for(let i = 0; i<anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
-        console.log("event - click on ", anchors[i]);
+        console.log("event - click on", anchors[i]);
         setDetails(anchors[i]);
         showDetails();
     })
 }
-function showDetails() {
+button = button.addEventListener("click", function() {
+    hideDetails();
+})
+
+function showDetails () {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function () {
+        detailsContainer.classList.remove(IS_POINT);
+    }, 1)
+    detailsAudio.play();
+    setTimeout(function () {
+        detailsAudio.pause();
+    }, 15000)
+
 }
+
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
 }
