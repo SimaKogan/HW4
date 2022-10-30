40c6ef2023c7e44ff03a707eb4e93404496b761f
SimaKogan <simas96@mail.ru>
2022-10-30 07:30:31 +0200

    HW9

--- src/index.js
+++ src/index.js
@@ -3,37 +3,20 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
-const detailsContainer = document.querySelector(".details-container");
-const detailsAudio = document.querySelector(".details-audio");
 const HIDDEN = "hidden";
-const button = document.querySelector(".hide-button");
-const IS_POINT = "is-point";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
-    detailsAudio.setAttribute('src', anchor.getAttribute('data-details-audio'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
-for(let i = 0; i<anchors.length; i++) {
+for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on", anchors[i]);
+        ShowDetails();
         setDetails(anchors[i]);
-        showDetails();
     })
 }
-button = button.addEventListener("click", function() {
-    hideDetails();
-})
-function showDetails () {
+function ShowDetails() {
     mainClass.classList.remove(HIDDEN);
-    detailsContainer.classList.add(IS_POINT);
-    setTimeout(function () {
-        detailsContainer.classList.remove(IS_POINT);
-    }, 1)
-    detailsAudio.play();
-    setTimeout(function () {
-        detailsAudio.pause();
-    }, 15000)
-
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
